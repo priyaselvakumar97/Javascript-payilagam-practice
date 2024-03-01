0007b7c183187b17fd1b368cf36880abad216e9d
priyaselvakumar97 <priyaselvakumar276@gmail.com>
2024-03-01 15:32:21 +0530

    Automatic slide show

--- script.js
+++ script.js
@@ -93,71 +93,71 @@ function sendsms(){
         }
 }
 
-function send(i){
-    x=document.getElementById("i1");
-    switch(i){
-    case 0: x.value+=0; break;
-    case 1: x.value+=1;break;
-    case 2: x.value+=2;break;
-    case 3: x.value+=3;break;
-    case 4: x.value+=4;break;
-    case 5: x.value+=5;break;
-    case 6: x.value+=6;break;
-    case 7: x.value+=7;break;
-    case 8: x.value+=8;break;
-    case 9: x.value+=9;break;
+// function send(i){
+//     x=document.getElementById("i1");
+//     switch(i){
+//     case 0: x.value+=0; break;
+//     case 1: x.value+=1;break;
+//     case 2: x.value+=2;break;
+//     case 3: x.value+=3;break;
+//     case 4: x.value+=4;break;
+//     case 5: x.value+=5;break;
+//     case 6: x.value+=6;break;
+//     case 7: x.value+=7;break;
+//     case 8: x.value+=8;break;
+//     case 9: x.value+=9;break;
 
 
-    case '+':x.value+='+';break;
-    case '-':x.value+='-';break;
-    case '*':x.value+='*';break;
-    case '/':x.value+='/';break;
-    case '.':x.value+='.';break;
-    case 'AC':x.value=' ';break;
+//     case '+':x.value+='+';break;
+//     case '-':x.value+='-';break;
+//     case '*':x.value+='*';break;
+//     case '/':x.value+='/';break;
+//     case '.':x.value+='.';break;
+//     case 'AC':x.value=' ';break;
 
-    case 'log':result=eval(Math.log10(x.value));x.value=result;
-    break;
-    case '√': result=eval(Math.sqrt(x.value));x.value=result;
-    break;
-    case '1/x': result=eval(1/x.value);
-    x.value=result;
-    break;
-    case '=':result=eval(x.value);
-    x.value=result;break;
+//     case 'log':result=eval(Math.log10(x.value));x.value=result;
+//     break;
+//     case '√': result=eval(Math.sqrt(x.value));x.value=result;
+//     break;
+//     case '1/x': result=eval(1/x.value);
+//     x.value=result;
+//     break;
+//     case '=':result=eval(x.value);
+//     x.value=result;break;
     
 
 
-    }
-}
+//     }
+// }
 
-  x=["img/1.jpg","img/2.jpg","img/3.jpg"];
-  slide=document.getElementById("m1");
-  count=-1;
-  function forward(){
-    count=count+1; //count=0
-    {
-        if(count==x.length) //0,1,2,3
-        {
-            count=0;
-        }
-        slide.src=x[count];
-    }
-    console.log(count);
-  }
+//   x=["img/1.jpg","img/2.jpg","img/3.jpg"];
+//   slide=document.getElementById("m1");
+//   count=-1;
+//   function forward(){
+//     count=count+1; //count=0
+//     {
+//         if(count==x.length) //0,1,2,3
+//         {
+//             count=0;
+//         }
+//         slide.src=x[count];
+//     }
+//     console.log(count);
+//   }
 
-  function backward()
-  {
-    count=count-1; //count=0
-    if(count<=x.length)// 0,1,2,3
-    {
-        if(count==-1 || count==-2)
-        {
-            count=2;
-        }
-        slide.src=x[count];//2,1,0
-    }
-    console.log(count)
-  }
+//   function backward()
+//   {
+//     count=count-1; //count=0
+//     if(count<=x.length)// 0,1,2,3
+//     {
+//         if(count==-1 || count==-2)
+//         {
+//             count=2;
+//         }
+//         slide.src=x[count];//2,1,0
+//     }
+//     console.log(count)
+//   }
 
 // //   refresh,reload
 // function Refresh()
@@ -177,8 +177,8 @@ function send(i){
 
 
         // prompt,alert and confirm
-        function check()
-        {
+        // function check()
+        // {
             // alert("Don't visit this page")
 
             // if (confirm("sure to proceed")){
@@ -196,14 +196,28 @@ function send(i){
 
 
             // }
-                password=prompt("Enter your password"," ");
-                if(password==1234){
-                    document.getElementById("p1").innerHTML="Thank you!!!";
-                }else{
-                    document.getElementById("p1").innerHTML="Oops,Sorry!!";
+        //         password=prompt("Enter your password"," ");
+        //         if(password==1234){
+        //             document.getElementById("p1").innerHTML="Thank you!!!";
+        //         }else{
+        //             document.getElementById("p1").innerHTML="Oops,Sorry!!";
+        //         }
+        // }
+ 
+            x=["img/1.jpg","img/2.jpg","img/3.jpg"];
+            slide=document.getElementById("m1");
+            count=-1;
+
+            function forward(){
+                count=count+1;
+                if(count<=x.length)
+                {
+                    if(count==x.length)
+                    {
+                        count=0;
+                    }
+                    slide.src=x[count];
                 }
-          
-
-
-        }
-
+                console.log(count);
+            }
+     setInterval(forward,2000);
